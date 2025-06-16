76f1b7fed3a35cb1df251b88b41859746058e955
salmanlashkarara <salmanlashkarara@gmail.com>
2025-06-16 13:20:04 +0200

    Rename the test

--- src/test/kotlin/com/saucedemo/WhenAuthenticateTest.kt
+++ src/test/kotlin/com/saucedemo/WhenAuthenticateTest.kt
@@ -4,7 +4,7 @@ import net.serenitybdd.annotations.Managed
 import net.serenitybdd.annotations.Steps
 import net.serenitybdd.core.Serenity
 import net.serenitybdd.junit5.SerenityJUnit5Extension
-import org.example.com.saucedemo.actions.LoginActions
+import org.example.com.saucedemo.actions.AuthenticationActions
 import org.example.com.saucedemo.pages.LoginPage
 import org.example.com.saucedemo.utility.UserType
 import org.junit.jupiter.api.Assertions.assertTrue
@@ -13,22 +13,24 @@ import org.junit.jupiter.api.extension.ExtendWith
 import org.openqa.selenium.WebDriver
 
 @ExtendWith(SerenityJUnit5Extension::class)
-class WhenLoginOnTest {
+class WhenAuthenticateTest {
 
     @Managed
     lateinit var driver: WebDriver
 
     @Steps
-    lateinit var loginActions: LoginActions
+    lateinit var authenticationActions: AuthenticationActions
 
     lateinit var loginPage: LoginPage
 
     @Test
-    fun userCanLogOnViaHomePage() {
-        loginActions.login("https://www.saucedemo.com/", UserType.STANDARD_USER)
+    fun userCanLogOnAndLogOut() {
+        authenticationActions.login("https://www.saucedemo.com/", UserType.STANDARD_USER)
 
         Serenity.reportThat("Title of the page should be Products") {
             assertTrue(loginPage.getPageTitle().contains("Products"))
         }
+
+        authenticationActions.logout()
     }
 }
