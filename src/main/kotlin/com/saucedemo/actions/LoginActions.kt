a5ef79f42acd143405438392ca64327e2d34b721
salmanlashkarara <salmanlashkarara@gmail.com>
2025-06-16 09:16:57 +0200

    Add actions

---
+++ src/main/kotlin/com/saucedemo/actions/LoginActions.kt
@@ -0,0 +1,19 @@
+package org.example.com.saucedemo.actions
+
+import net.serenitybdd.annotations.Step
+import net.serenitybdd.core.steps.UIInteractionSteps
+import org.example.com.saucedemo.pages.LoginPage
+import org.example.com.saucedemo.utility.UserType
+
+open class LoginActions : UIInteractionSteps() {
+
+    private val loginPage: LoginPage = LoginPage()
+
+    @Step("Login as {1}")
+    fun login(url: String, user: UserType) {
+        openUrl(url)
+        loginPage.insertUsername(user.username)
+        loginPage.insertPassword(user.password)
+        loginPage.clickLoginButton()
+    }
+}
